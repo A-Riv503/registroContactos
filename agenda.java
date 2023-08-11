03874fc2c5606544c611e8148e3c287209a017a0
Ariel <arielhernandz2004@gmail.com>
2023-08-11 13:15:46 -0600

    registro contactos

--- agenda.java
+++ agenda.java
@@ -1,11 +1,11 @@
 import javax.swing.*;
-import java.lang.runtime.SwitchBootstraps;
+
 
 
 public class agenda {
 
 
-    public static void ingresoContaco(int cantidadDeContactos){
+    /*public static void ingresoContaco(int cantidadDeContactos){
         int continuar;
         do{
             String nombre,correo;
@@ -27,7 +27,7 @@ public class agenda {
             }
 
         }while(continuar==1);
-    }
+    }*/
     public static void mostrarContacto(contacto informacion[],int cantidad){
         for(int i=0; i<cantidad;i++){
             System.out.println(informacion[i].nombre);
@@ -43,7 +43,8 @@ public class agenda {
             case 1:
                 String nombreContactoBuscado=JOptionPane.showInputDialog("ingrese el nombre de su contacto");
                 for(int i=0;i<cantidadContactos;i++){
-                    if(informacionContacto[i].nombre==nombreContactoBuscado){
+                    if(informacionContacto[i].nombre.equals(nombreContactoBuscado)){
+                        System.out.println("esta es toda la informacion del contacto");
                         System.out.println(informacionContacto[i].nombre);
                         System.out.println(informacionContacto[i].correo);
                         System.out.println(informacionContacto[i].numeroTelefono);
@@ -55,7 +56,8 @@ public class agenda {
             case 2:
                 String correoBuscado=JOptionPane.showInputDialog("ingrese el correo que busca");
                 for(int i=0;i<cantidadContactos;i++){
-                    if(informacionContacto[i].correo==correoBuscado){
+                    if(informacionContacto[i].correo.equals(correoBuscado)){
+                        System.out.println("esta es toda la informacion del contacto");
                         System.out.println(informacionContacto[i].nombre);
                         System.out.println(informacionContacto[i].correo);
                         System.out.println(informacionContacto[i].numeroTelefono);
@@ -68,6 +70,7 @@ public class agenda {
                 int numeroBuscado=Integer.parseInt(JOptionPane.showInputDialog("ingrese el numero de su contacto"));
                 for(int i=0;i<cantidadContactos;i++){
                     if(informacionContacto[i].numeroTelefono==numeroBuscado){
+                        System.out.println("esta es toda la informacion del contacto");
                         System.out.println(informacionContacto[i].nombre);
                         System.out.println(informacionContacto[i].correo);
                         System.out.println(informacionContacto[i].numeroTelefono);
@@ -88,7 +91,27 @@ public class agenda {
 
 
         int cantidadDeContactos=Integer.parseInt(JOptionPane.showInputDialog("cuantos contactos desea ingresar"));
-        ingresoContaco(cantidadDeContactos);
+        int continuar;
+        do{
+            String nombre,correo;
+            int numeroDeTelefono;
+            contacto informacionContacto[]=new contacto[cantidadDeContactos];
+            for(int i=0;i<cantidadDeContactos;i++){
+                nombre=JOptionPane.showInputDialog("nombre de contacto");
+                correo=JOptionPane.showInputDialog("ingrese el correo");
+                numeroDeTelefono=Integer.parseInt(JOptionPane.showInputDialog("dijite el numero de telefono"));
+                informacionContacto[i]=new contacto(nombre,correo,numeroDeTelefono);
+            }
+
+            continuar=Integer.parseInt(JOptionPane.showInputDialog
+                    ("Desea hacer otra accion 1-agregar mas contactos 2-mostrar lista de contactos 3-buscar contacto 4-finalizar programa"));
+            if(continuar==2){
+                mostrarContacto(informacionContacto,cantidadDeContactos);
+            }else if(continuar==3){
+                buscarContacto(informacionContacto,cantidadDeContactos);
+            }
+
+        }while(continuar==1);
 
 
 
